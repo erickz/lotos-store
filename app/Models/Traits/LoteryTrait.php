3d6293399b4f0d170404612485aef94a967cf21f
Erick Carvalho de São Miguel <erick@nitmedia.com>
2025-04-04 13:15:07 -0300

    Fixed lotery data

--- app/Models/Traits/LoteryTrait.php
+++ app/Models/Traits/LoteryTrait.php
@@ -19,13 +19,13 @@ trait LoteryTrait
     public function getFormatedDrawDays()
     {
         $daysWeek = [
-            '01' => 'Domingo'
-            ,'02' => 'Segunda'
-            ,'03' => 'Terça'
-            ,'04' => 'Quarta'
-            ,'05' => 'Quinta'
-            ,'06' => 'Sexta'
-            ,'07' => 'Sábado'
+            '1' => 'Domingo'
+            ,'2' => 'Segunda'
+            ,'3' => 'Terça'
+            ,'4' => 'Quarta'
+            ,'5' => 'Quinta'
+            ,'6' => 'Sexta'
+            ,'7' => 'Sábado'
         ];
 
 
