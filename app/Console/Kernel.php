11a890a1f2dd2c09349e8e9d25ee980e06a1d864
Erick Carvalho de São Miguel <erick@nitmedia.com>
2025-04-06 15:30:11 -0300

    Updated the cron times

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,8 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('create:concursos-for-week')->weekly();
-        $schedule->command('check:concursos-of-day')->dailyAt('20:05');
-        $schedule->command('check:boloes-of-day')->dailyAt('20:10');
+        $schedule->command('check:concursos-of-day')->dailyAt('20:15');
+        $schedule->command('check:boloes-of-day')->dailyAt('20:20');
         // $schedule->command('inspire')->hourly();
     }
 
